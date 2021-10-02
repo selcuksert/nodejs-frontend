854d39c2eee23a99c7358cc2b6b8884baae55580
selcuksert <selcuk.sert@gmail.com>
2021-10-02 20:30:01 +0300

    Updated data schema to include hostname

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,45 +3,46 @@ import Post from './Post'
 import Container from 'react-bootstrap/Container'
 
 class PostList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: null,
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: null,
+        }
+        this.updatePosts = this.updatePosts.bind(this)
     }
-    this.updatePosts = this.updatePosts.bind(this)
-  }
 
-  componentDidMount() {
-    fetch('posts')
-      .then(response => response.json())
-      .then(data => this.updatePosts(data))
-  }
-
-  render() {
-    return (
-      <Container>
-        {this.getPosts()}
-      </Container>
-    )
-  }
-
-  updatePosts(posts) {
-    if (Array.isArray(posts)) {
-      this.setState({
-        posts: posts
-      })
+    componentDidMount() {
+        fetch('posts')
+            .then(response => response.json())
+            .then(data => this.updatePosts(data))
     }
-  }
 
-  getPosts() {
-    if (this.state.posts == null) {
-      return <div> waiting </div>
-    } else {
-      return this.state.posts.map((post) =>
-        <Post id={post.id} title={post.title} content={post.content} timestamp={post.timestamp} key={post.timestamp} hostname={post.hostname}/>
-      )
+    render() {
+        return (
+            <Container>
+                {this.getPosts()}
+            </Container>
+        )
     }
-  }
+
+    updatePosts(posts) {
+        if (Array.isArray(posts)) {
+            this.setState({
+                posts: posts
+            })
+        }
+    }
+
+    getPosts() {
+        if (this.state.posts == null) {
+            return <div> waiting </div>
+        } else {
+            return this.state.posts.map((post) =>
+                <Post id={post.id} title={post.title} content={post.content} timestamp={post.timestamp}
+                      key={post.timestamp} hostname={post.hostname}/>
+            )
+        }
+    }
 }
 
 export default PostList
